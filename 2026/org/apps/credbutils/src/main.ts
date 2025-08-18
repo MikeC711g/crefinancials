bd0aef0814cc94f3b8d9dac523eea1576f8d7302
Michael Casile <mikec711g@gmail.com>
2025-08-18 13:57:42 -0400

    Beginning of module to standalone conversion

--- 2026/org/apps/credbutils/src/main.ts
+++ 2026/org/apps/credbutils/src/main.ts
@@ -1,12 +1,44 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, Firestore, initializeFirestore, connectFirestoreEmulator, getFirestore } from '@angular/fire/firestore';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule, 
+        // provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => {
+            console.log('Env Emulators', environment.useEmulators);
+            const auth = getAuth();
+            if (environment.useEmulators) {
+                connectAuthEmulator(auth, 'http://localhost:9099', {
+                    disableWarnings: true
+                });
+            }
+            return auth;
+        }), provideFirestore(() => {
+            let fireStore: Firestore;
+            if (environment.useEmulators) {
+                fireStore = initializeFirestore(getApp(), {
+                    experimentalForceLongPolling: true
+                });
+                connectFirestoreEmulator(fireStore, 'localhost', 8080);
+            }
+            else {
+                fireStore = getFirestore();
+            }
+            return fireStore;
+        }))]
+})
   .catch(err => console.error(err));
